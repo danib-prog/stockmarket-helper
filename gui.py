d108f81f6dce0a81220b0cd409ebd34d882c6c18
danib-prog <9buza.dani@gmail.com>
2021-03-15 20:43:27 +0100

    Defined a window class for the request of the API key and made progress in the main UI.

--- gui.py
+++ gui.py
@@ -2,15 +2,43 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-from PyQt5 import uic
-from aspects import Ui_Dialog
 
-aspects = ["Kiskunfelegyhaza" for x in range(200)]
+apikey = None
+
+class ApiKeyWindow(QMainWindow):
+    def __init__(self):
+        super(ApiKeyWindow, self).__init__()
+
+        layout = QHBoxLayout()
+        layout.addWidget(QLabel("Your Alpha Vantage API key: "))
+        self.lineedit = QLineEdit()
+        self.lineedit.returnPressed.connect(self.onOkButtonClicked)
+        layout.addWidget(self.lineedit)
+        self.okbutton = QPushButton('Ok')
+        self.okbutton.clicked.connect(self.onOkButtonClicked)
+        layout.addWidget(self.okbutton)
+
+        centralwidget = QWidget()
+        centralwidget.setLayout(layout)
+        self.setCentralWidget(centralwidget)
+
+    def onOkButtonClicked(self, s):
+        global apikey
+        apikey = self.lineedit.text()
+        self.close()
+
+import mainwindow
+import stockselector
+import aspects
+
+mainwindow_ui = mainwindow.Ui_MainWindow
+stockselector_ui = stockselector.Ui_Dialog
+aspects_ui = aspects.Ui_Dialog
+
 
 def aspectsIntoLayout(aspects):
     tristates = ['Symbol', 'Currency']
-    rowcount = (len(aspects) / 2) + 1
-    rowcount = round(rowcount)
+    rowcount = (len(aspects) // 2) + 1
 
     layout = QGridLayout()
     layout.addWidget(QLabel('  '), rowcount, 2)
@@ -18,26 +46,53 @@ def aspectsIntoLayout(aspects):
     for a in aspects:
         if a not in tristates:
             checkbox = QCheckBox(a)
-            checkbox.setCheckState(True)
+            checkbox.setCheckState(2)
             layout.addWidget(checkbox)
 
     return layout
 
-class MainWindow(QMainWindow):
-    def __init__(self, aspects, *args, **kwargs):
+
+class MainWindow(QMainWindow, mainwindow_ui):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.setupUi(self)
 
-        self.aspects = aspects
+        self.toolBar.addWidget(QLabel('Sort by:'))
+        sortby_combo = QComboBox()
+        sortby_combo.addItems(["Graham's number", "Market Capitalization",
+                               "Number of shares", "Total Assets"])
+        sortby_combo.currentIndexChanged.connect(self.onSortByComboIndexChange)
+        sortby_combo.setStatusTip("A selection menu for on what aspects the sorting is based")
+        sortby_combo.setToolTip("Aspects to sort by")
+        self.toolBar.addWidget(sortby_combo)
 
-        button = QPushButton("Click me!")
-        button.clicked.connect(self.buttonClicked)
-        self.setCentralWidget(button)
+        self.actionBusinesses.setIcon(QIcon("./icons-shadowless-32/document-text.png"))
+        self.actionBusinesses.triggered.connect(self.onActionBusinessesTriggered)
 
-    def buttonClicked(self, s):
-        dlg = AspectsDialog(self.aspects)
-        dlg.exec_()
+        self.actionAspectsShown.setIcon(QIcon("./icons-shadowless-32/table.png"))
+        self.actionAspectsShown.triggered.connect(self.onActionAspectsTriggered)
 
-class AspectsDialog(QDialog, Ui_Dialog):
+    def onActionBusinessesTriggered(self, s):
+        pass
+
+    def onActionAspectsTriggered(self, s):
+        pass
+
+    def onSortByComboIndexChange(self, i):
+        index_to_aspect = {0: 'Graham Number',
+                           1: 'MarketCapitalization',
+                           2: 'SharesOutstanding',
+                           3: 'totalAssets'}
+
+
+class StockSelectorDialog(QDialog, stockselector_ui):
+    def __init__(self, symbols, *args, **kwargs):
+        super(StockSelectorDialog, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.symbols = symbols
+
+
+class AspectsDialog(QDialog, aspects_ui):
     def __init__(self, aspects, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
@@ -48,6 +103,7 @@ class AspectsDialog(QDialog, Ui_Dialog):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainWindow(aspects)
+    window = ApiKeyWindow()
     window.show()
     app.exec_()
+
