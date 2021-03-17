4ba342912f21e3912f29a69f31d600b0b1ee1af6
danib-prog <9buza.dani@gmail.com>
2021-03-17 23:06:30 +0100

    Finished the aspects and symbols dialogs, including connection to MainWindow

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+import scraper
 
 apikey = None
 
@@ -36,26 +37,22 @@ stockselector_ui = stockselector.Ui_Dialog
 aspects_ui = aspects.Ui_Dialog
 
 
-def aspectsIntoLayout(aspects):
-    tristates = ['Symbol', 'Currency']
-    rowcount = (len(aspects) // 2) + 1
 
-    layout = QGridLayout()
-    layout.addWidget(QLabel('  '), rowcount, 2)
-
-    for a in aspects:
-        if a not in tristates:
-            checkbox = QCheckBox(a)
-            checkbox.setCheckState(2)
-            layout.addWidget(checkbox)
-
-    return layout
 
 
 class MainWindow(QMainWindow, mainwindow_ui):
+    aspects_ = scraper.aspects
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
+
+        self.symbols = []
+        self.symbolmodel = SymbolListModel(self)
+
+        self.aspects_shown = []
+        for a in self.aspects_:
+            tup = (a, 2)
+            self.aspects_shown.append(tup)
 
         self.toolBar.addWidget(QLabel('Sort by:'))
         sortby_combo = QComboBox()
@@ -73,10 +70,14 @@ class MainWindow(QMainWindow, mainwindow_ui):
         self.actionAspectsShown.triggered.connect(self.onActionAspectsTriggered)
 
     def onActionBusinessesTriggered(self, s):
-        pass
+        dlg = StockSelectorDialog(self)
+        dlg.setWindowTitle("Businesses Selector")
+        dlg.exec_()
 
     def onActionAspectsTriggered(self, s):
-        pass
+        dlg = AspectsDialog(self)
+        dlg.setWindowTitle("Aspects' Selector")
+        dlg.exec_()
 
     def onSortByComboIndexChange(self, i):
         index_to_aspect = {0: 'Graham Number',
@@ -85,25 +86,82 @@ class MainWindow(QMainWindow, mainwindow_ui):
                            3: 'totalAssets'}
 
 
+class SymbolListModel(QAbstractListModel):
+    def __init__(self, parent_, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.symbols = parent_.symbols
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            symbol = self.symbols[index.row()]
+            return symbol
+
+    def rowCount(self, index):
+        return len(self.symbols)
+
+
+
 class StockSelectorDialog(QDialog, stockselector_ui):
-    def __init__(self, symbols, *args, **kwargs):
+    def __init__(self, parent_, *args, **kwargs):
         super(StockSelectorDialog, self).__init__(*args, **kwargs)
         self.setupUi(self)
-        self.symbols = symbols
+        self.symbols = parent_.symbols
+
+        self.symbolmodel = parent_.symbolmodel
+        self.listView.setModel(parent_.symbolmodel)
+
+        self.symbolAddButton.clicked.connect(self.onAddButtonClicked)
+        self.symbolDeleteButton.clicked.connect(self.onDeleteButtonClicked)
+
+    def onAddButtonClicked(self, s):
+        symbol = self.symbolEdit.text()
+        if symbol:
+            self.symbols.append(symbol)
+            self.symbolmodel.layoutChanged.emit()
+            self.symbolEdit.setText("")
+
+    def onDeleteButtonClicked(self, s):
+        indexes = self.listView.selectedIndexes()
+        if indexes:
+            for i in indexes:
+                del self.symbols[i.row()]
+
+            self.symbolmodel.layoutChanged.emit()
+            self.listView.clearSelection()
 
 
 class AspectsDialog(QDialog, aspects_ui):
-    def __init__(self, aspects, *args, **kwargs):
+    def __init__(self, parent, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.parent_ = parent
         self.setupUi(self)
 
-        layout = aspectsIntoLayout(aspects)
+        layout = self.aspectsIntoLayout()
         self.aspectsWidget.setLayout(layout)
+
+    def aspectsIntoLayout(self):
+        tristates = ['Symbol', 'Currency']
+        rowcount = (len(self.parent_.aspects_shown) // 2) + 1
+
+        layout = QGridLayout()
+        layout.addWidget(QLabel('  '), rowcount, 2)
+
+        for i, a in enumerate(self.parent_.aspects_shown):
+            if a not in tristates:
+                checkbox = QCheckBox(a[0])
+                checkbox.setCheckState(a[1])
+                checkbox.stateChanged.connect(lambda s, i=i, aspect=a[0]: self.checkSateChanged(s, i, aspect))
+                layout.addWidget(checkbox)
+
+        return layout
+
+    def checkSateChanged(self, state, index, aspect):
+        self.parent_.aspects_shown[index] = (aspect, state)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = ApiKeyWindow()
+    window = MainWindow()
     window.show()
     app.exec_()
 
--- scraper.py
+++ scraper.py
@@ -5,7 +5,17 @@ import time
 import numpy as np
 
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'}
-apikey = input("What's your Alpha Vantage API key? ")
+aspects = ('AssetType', 'Description', 'Exchange', 'Country',
+           'Sector', 'Industry', 'Address', 'FullTimeEmployees',
+           'FiscalYearEnd', 'PEGRatio', 'OperatingMarginTTM',
+           'ReturnOnEquityTTM', 'DilutedEPSTTM', 'AnalystTargetPrice',
+           'ForwardPE', 'PriceToSalesRatioTTM', 'EVToEBITDA',
+           'Beta', '50DayMovingAverage', '200DayMovingAverage',
+           'SharesFloat', 'SharesShort', 'SharesShortPriorMonth',
+           'ShortRatio', 'ShortPercentOutstanding', 'ShortPercentFloat',
+           'ForwardAnnualDividendRate', 'ForwardAnnualDividendYield',
+           'DividendDate', 'ExDividendDate', 'LastSplitFactor',
+           'LastSplitDate')
 
 
 class Stock():
